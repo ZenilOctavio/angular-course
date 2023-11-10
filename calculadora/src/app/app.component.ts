4fe3cf74ee48a95e7b70ab5ddbb27db9cb21d763
Octavio Zenil <octaviozenil@gmail.com>
2023-11-10 07:35:27 -0700

    Aplicacion calculadora terminada

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -11,4 +11,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'calculadora';
+  resultado = ''
+
+  sumar(event: Event, cadena1: string, cadena2: string){
+    event.preventDefault()
+    
+    let numero1 = Number(cadena1)
+    let numero2 = Number(cadena2)
+
+    try{
+      this.resultado = `El resultado es: ${numero1 + numero2}`
+    }
+    catch(err){
+      this.resultado = 'No introduciste valores válidos'
+    }
+  }
 }
